384a2249ab7dbc8a8046db179f50becb6516ca6d
Fimasik <113976105+Fimav2@users.noreply.github.com>
2023-03-05 12:20:57 +0300

    Update middle_list.h

--- middle_list.h
+++ middle_list.h
@@ -1,6 +1,10 @@
 #ifndef MIDDLE_LIST_H_INCLUDED
 #define MIDDLE_LIST_H_INCLUDED
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
+
 
 vector <char> itc_strtlist(string str);//1
 
