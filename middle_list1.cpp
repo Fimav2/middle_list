88056f0410f79d29d8df33784bab991fae459b35
Я сын шлюхи <113976105+Fimav2@users.noreply.github.com>
2023-03-12 22:22:36 +0300

    Update middle_list1.cpp

--- middle_list1.cpp
+++ middle_list1.cpp
@@ -35,3 +35,70 @@ void itc_odd_even_separator_lst(const vector <int> &lst, vector <int> &lst1, vec
 
 
 }
+void itc_pos_neg_separator_lst(const vector <int> &lst, vector <int> &lst1, vector <int> &lst2, vector <int> &lst3){//9
+    for(int i = 0;i < lst.size();i++){
+        if(lst[i] < 0)
+            lst1.push_back(lst[i]);
+        else if(lst[i] == 0)
+                lst2.push_back(lst[i]);
+
+        else if(lst[i] > 0)
+                lst3.push_back(lst[i]);
+
+    }
+}
+
+void itc_odd_even_analysis_lst(const vector <int> &lst){//10
+    cout << "ANALIZATOR:" << endl;
+    int sum = 0;
+    for(int i = 0;i < lst.size();i++){
+        if(lst[i]%2 == 0)
+            sum++;
+    }
+    cout << "kol chet:" << sum << ","<< "    ";
+    int nesum = 0;
+    for(int i = 0;i < lst.size();i++)
+        if(lst[i]%2 != 0)
+            nesum++;
+    cout << "kol ne chet:" << nesum << "," << endl;
+    int maxi = 0;
+    for(int i = 0;i < lst.size();i++){
+        if (lst[i] % 2 == 0)
+            if (lst[i] > maxi)
+            maxi = lst[i];
+    }
+    cout << "max chet:"  <<  maxi  << "," << "    ";
+    int max = 0;
+    for(int i = 0;i < lst.size();i++){
+        if (lst[i] % 2 != 0)
+            if (lst[i] > max)
+            max = lst[i];
+    }
+    cout << "max ne chet:" << max << "," << endl;
+    int mini = 10000000000;
+    for(int i = 0;i < lst.size();i++){
+        if (lst[i] % 2 == 0)
+            if (lst[i] < mini)
+            mini = lst[i];
+    }
+    cout << "min chet:" << mini << "," << "    ";
+    int min = 10000000000;
+    for(int i = 0;i < lst.size();i++){
+        if (lst[i] % 2 != 0)
+            if (lst[i] < min)
+            min = lst[i];
+    }
+    cout << "min ne chet:" << min << "," << endl;
+    int summa = 0;
+    for(int i = 0; i < lst.size();i++){
+        if(lst[i]%2 == 0)
+            summa = summa + lst[i];
+    }
+    cout << "summa chet:" << summa << "," << "   ";
+     int nesumma = 0;
+    for(int i = 0; i < lst.size();i++){
+        if(lst[i]%2 != 0)
+            nesumma = nesumma + lst[i];
+    }
+    cout << "summa ne chet:" << nesumma << "," << endl;
+}
